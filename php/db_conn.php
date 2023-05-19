a9fe1228e9d4c8966961be38c8429b0c78f2b89c
Govarthenan Rajadurai <govarthenan@gmail.com>
2023-05-19 23:01:09 +0530

    Change database name

--- php/db_conn.php
+++ php/db_conn.php
@@ -2,7 +2,7 @@
     $host = "localhost";
     $user = "root";
     $pass = "";
-    $db = "wdp";
+    $db = "jobvertex";
     $conn = mysqli_connect($host, $user, $pass, $db);
     if(!$conn){
         die("Connection Failed: " . mysqli_connect_error());
