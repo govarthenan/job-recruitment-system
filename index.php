6c6a9070424424c3a10ee19923bb507890a9f0c1
Govarthenan Rajadurai <govarthenan@gmail.com>
2023-05-19 18:20:28 +0530

    Refactor

--- index.php
+++ index.php
@@ -11,15 +11,15 @@ include("./php/navigator.php");
 	<meta charset="UTF-8">
 	<meta http-equiv="X-UA-Compatible" content="IE=edge">
 	<meta name="viewport" content="width=device-width, initial-scale=1.0">
-	<title>Welcome to our system hello!</title>
-	<link rel=" stylesheet" type="text/css" href="./css/style.css">
+	<title>Welcome to our system!</title>
+	<link rel="stylesheet" type="text/css" href="./css/style.css">
 	<link rel="stylesheet" href="./css/navbar.css">
-	<link rel="stylesheet" href="./css/footer.css"> 
-<style>
-	main{
-		height:100%;
-	}
-</style>
+	<link rel="stylesheet" href="./css/footer.css">
+	<style>
+		main {
+			height: 100%;
+		}
+	</style>
 </head>
 
 <body>
